d7b05a3b0d7dd8d2c8aa18f17b7c9acb8cb06231
nitraxx07-source <nitraxx07@gmail.com>
2026-09-18 12:44:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ def eliminar_usuario(cedula_objetivo):
         if conn:
             conn.close()
 
-# 5. OBTENER REPORTES
+# 5. OBTENER REPORTES (CORREGIDO CON r.created_at)
 @app.route('/api/v1/reportes', methods=['GET'])
 def obtener_reportes():
     conn = None
@@ -262,6 +262,7 @@ def obtener_reportes():
                 r.tipo_alerta,
                 r.estado,
                 r.gps,
+                r.created_at,
                 COALESCE(r.barrio, u.barrio) AS barrio,
                 COALESCE(NULLIF(r.nombre_completo, ''), u.nombres || ' ' || u.apellidos, 'Vecino') AS nombre_completo,
                 TRIM(r.cedula_vecino::text) AS cedula_vecino,
